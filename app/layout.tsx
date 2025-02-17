f350a1fe9676ab744634c8bbc8fe825950eec8f4
rasoul707 <r.ahmadifar.1377@gmail.com>
2025-02-18 00:15:09 +0330

    fix complete

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,24 @@
-import type {Metadata} from "next";
+import type {Metadata, Viewport} from "next";
 import "./globals.css";
 import {Providers} from "@/app/providers";
 import {IRANSansX} from "@/lib/font";
 
 
 export const metadata: Metadata = {
-    title: "Instagram Bot",
+    title: "ربات اددر کلوزفرند",
 };
+
+export const viewport: Viewport = {
+    themeColor: [
+        {media: "(prefers-color-scheme: light)", color: "#FF921F"},
+        {media: "(prefers-color-scheme: dark)", color: "#FF921F"},
+    ],
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+    userScalable: false,
+}
+
 
 export default function RootLayout({children}: Readonly<{ children: React.ReactNode; }>) {
     return (
